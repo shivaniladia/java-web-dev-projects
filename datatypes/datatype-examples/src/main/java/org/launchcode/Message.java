a82c84c865afeab373ccfb32b0efba39e1719fb8
Shivani Ladia <154580687+shivaniladia@users.noreply.github.com>
2024-05-02 19:30:47 -0500

    Initial commit for studio

--- datatypes/datatype-examples/src/main/java/org/launchcode/Message.java
+++ datatypes/datatype-examples/src/main/java/org/launchcode/Message.java
@@ -3,14 +3,16 @@ package org.launchcode;
 public class Message {
 
     public static String getMessage(String lang, String customMessage) {
-        if (lang.equals("sp")) {
-            return customMessage; // Return the custom message for Spanish
-        } else if (lang.equals("fr")) {
-            return customMessage; // Return the custom message for French
-        } else if (lang.equals("hi")) {
-            return "Namaste, Dunia!"; // Return the custom message for Hindi
-        }
-        return customMessage; // Return the custom message for other languages or the default message
+        return switch (lang) {
+            case "sp" -> customMessage; // Return the custom message for Spanish
+
+            case "fr" -> customMessage; // Return the custom message for French
+
+            case "hi" -> "Namaste, Dunia!";
+            default -> // Return the custom message for Hindi
+
+                    customMessage;
+        };
     }
     }
 
