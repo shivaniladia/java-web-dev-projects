41ccc97057f7bb43e54edd5c0bbd1236ad637597
Shivani Ladia <154580687+shivaniladia@users.noreply.github.com>
2024-05-06 11:59:57 -0500

    Initial commit for studio

--- datatypes/datatypes-studio/src/main/java/studios/areaofacircle/Circle.java
+++ datatypes/datatypes-studio/src/main/java/studios/areaofacircle/Circle.java
@@ -1,8 +1,21 @@
 package studios.areaofacircle;
 
+import java.util.Scanner;
+
 public class Circle {
 
-    public static Double getArea(Double radius) {
-        return 3.14 * radius * radius;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+
+        double radius = input.nextDouble();
+        input.close();
+       // double area = Math.PI * Math.pow(radius, 2);
+        double area = getArea(radius);
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
+
+    public static Double getArea(double radius) {
+        return Math.PI * Math.pow(radius, 2);
     }
 }
